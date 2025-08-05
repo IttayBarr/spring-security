7bed919877205a1a231e4eee2b20628ede1f8560
IttayBarr <ittaybarrientos@gmail.com>
2025-08-05 17:54:33 -0600

    refactor: cleanup unused imports in SecurityConfig

--- src/main/java/org/ittayb/springsecurity/security/SecurityConfig.java
+++ src/main/java/org/ittayb/springsecurity/security/SecurityConfig.java
@@ -1,6 +1,5 @@
 package org.ittayb.springsecurity.security;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,13 +9,8 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
